61d17c18bd87e3103c592032047303a36b50b340
unknown <eyangsd@gmail.com>
2018-09-18 20:22:45 -0700

    deploying to heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var app = express();
 // Configure middleware
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
